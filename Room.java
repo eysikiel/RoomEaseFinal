d0c12b58c50290d0497c5587d60d6873e4fe6a0d
eysikiel <lyka.lamparero@wvsu.edu.ph>
2025-11-11 16:00:21 +0800

    rara source action be acting up

--- Room.java
+++ Room.java
@@ -1,3 +1,20 @@
 public class Room {
+
+    private String roomID;
+    private String roomNumber;
+    private RoomType type;
+    private double price;
+    private int capacity;
+    private List<String> amenities = new LinkedList<>();
+    private RoomStatus status;
+    private PricingType pricingType;
+
     
+
+    public void displayInfo() {
+    }
+
+    public void updateInfo(Room updatedRoom) {
+    }
+
 }
