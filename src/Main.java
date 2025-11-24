5cab8a7456bab0dd2ba73662fe3707637e9f2db4
eysikiel <lyka.lamparero@wvsu.edu.ph>
2025-11-24 21:08:44 +0800

    update main

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import src.Model.User.User;
 
 public class Main {
     public static void main(String[] args) {
-        User landlord = new Landlord("09561620716", "Lyka", "Amaguin", "lykaamaguin", "USR01", "lyka", User.Role.LANDLORD);
+        User landlord = new Landlord("09561620716", "Lyka", "Lamparero", "lykalamparero", "USR01", "lyka", User.Role.LANDLORD);
         User.getUsers().add(landlord);
 
         landlord.displayLogInMenu();
